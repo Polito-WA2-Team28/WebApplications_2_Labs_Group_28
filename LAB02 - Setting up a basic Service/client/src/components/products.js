6c9adeba79dca12ef7e755510406202591ea18b9
Edoardo Colella s257113 <s257113@studenti.polito.it>
2023-03-28 18:35:13 +0200

    interfaccia aggiornata, bisogna decidere i campi prima di procedere

--- LAB02 - Setting up a basic Service/client/src/components/products.js
+++ LAB02 - Setting up a basic Service/client/src/components/products.js
@@ -1,17 +1,18 @@
 
 import { useState } from 'react';
-import { Button, Col, Container, Row } from 'react-bootstrap';
+import { Button, Col, Container, Form, Row } from 'react-bootstrap';
 import { API } from '../API';
 
 export function GetProducts() {
 
     const [products, setProducts] = useState([]);
+    const [error, setError] = useState();
 
     let getProducts = async () => {
         let products = [];
         await API.getAllProducts()
             .then(data => { products = data; setProducts(products) })
-            .catch(error => console.log(error));
+            .catch(error => setError("Error: " + error));
 
         return products;
     }
@@ -20,9 +21,10 @@ export function GetProducts() {
         <Container>
             <Row>
                 <Col>
-                    <Row><p style={{ textAlign: 'center' }}>GET /API/products/</p></Row>
+                    <Row ><p style={{ textAlign: 'center' }}>GET /API/products/</p></Row>
                     <Row><Button onClick={getProducts}>SEND REQUEST</Button></Row>
                     {products.length > 0 && products.map(product => <Row>{product.name}</Row>)}
+                    {error && <Row><Col>{error}</Col></Row>}
                 </Col>
             </Row>
         </Container>
@@ -33,12 +35,13 @@ export function GetProductById() {
 
     const [product, setProduct] = useState();
     const [id, setId] = useState(); 
+    const [error, setError] = useState();
 
     let getProduct = async () => {
         let product;
         await API.getProductById(id)
             .then(data => { product = data; setProduct(product) })
-            .catch(error => console.log(error));
+            .catch(error => setError("Error: " + error));
         return product;
     }
 
@@ -47,8 +50,17 @@ export function GetProductById() {
             <Row>
                 <Col>
                     <Row><p style={{ textAlign: 'center' }}>GET /API/products/:id</p></Row>
+                    <Row>
+                        <Form>
+                            <Form.Group className="mb-3" controlId="productID">
+                                <Form.Label>Product ID</Form.Label>
+                                <Form.Control type="text" placeholder="Enter product ID" onChange={e => setId(e.target.value)} />
+                            </Form.Group>
+                        </Form>
+                    </Row>
                     <Row><Button onClick={getProduct}>SEND REQUEST</Button></Row>
                     <Row>{product && <Col>{product.name}</Col>}</Row>
+                    {error && <Row><Col>{error}</Col></Row>}
                 </Col>
             </Row>
         </Container>
--- LAB02 - Setting up a basic Service/client/src/components/profiles.js
+++ LAB02 - Setting up a basic Service/client/src/components/profiles.js
@@ -1,16 +1,17 @@
 import { useState } from "react";
-import { Button, Col, Container, Row } from "react-bootstrap";
+import { Button, Col, Container, Form, Row } from "react-bootstrap";
 import { API } from "../API";
 
 export function GetProfileByEmailAddress() {
 
     const [profile, setProfile] = useState([]);
+    const [error, setError] = useState();
 
     let getProfile = async () => {
         let products = [];
         await API.getProfileByEmailAddress()
             .then(data => { products = data; setProfile(products) })
-            .catch(error => console.log(error));
+            .catch(error => setError("Error: " + error));
 
         return products;
     }
@@ -22,6 +23,8 @@ export function GetProfileByEmailAddress() {
                     <Row><p style={{ textAlign: 'center' }}>GET /API/profiles/:emailAddress</p></Row>
                     <Row><Button onClick={getProfile}>SEND REQUEST</Button></Row>
                     {profile && <Row>{profile.name}</Row>}
+                    {error && <Row><Col>{error}</Col></Row>}
+
                 </Col>
             </Row>
         </Container>
@@ -31,12 +34,16 @@ export function GetProfileByEmailAddress() {
 export function CreateProfile() {
 
     const [profile, setProfile] = useState();
+    const [email, setEmail] = useState();
+    const [name, setName] = useState();
+    const [surname, setSurname] = useState();
+    const [error, setError] = useState();
 
     let createProfile = async () => {
-        let profile;
+        let profile = { email: email, name: name, surname:surname}
         await API.createProfile(profile)
             .then(data => { profile = data; setProfile(profile)})
-            .catch(error => console.log(error));
+            .catch(error => setError("Error: " + error));
     }
 
     return <>
@@ -44,8 +51,26 @@ export function CreateProfile() {
             <Row>
                 <Col>
                     <Row><p style={{ textAlign: 'center' }}>POST /API/profiles/:emailAddress</p></Row>
+                    <Row>
+                        <Form>
+                            <Form.Group className="mb-3" controlId="email">
+                                <Form.Label>Email</Form.Label>
+                                <Form.Control type="email" placeholder="Enter email" onChange={e => setEmail(e.target.value)} />
+                            </Form.Group>
+                            <Form.Group className="mb-3" controlId="name">
+                                <Form.Label>Name</Form.Label>
+                                <Form.Control type="name" placeholder="Enter name" onChange={e => setName(e.target.value)} />
+                            </Form.Group>
+                            <Form.Group className="mb-3" controlId="surname">
+                                <Form.Label>Surname</Form.Label>
+                                <Form.Control type="surname" placeholder="Enter surname" onChange={e => setSurname(e.target.value)} />
+                            </Form.Group>
+                        </Form>
+                    </Row>
                     <Row><Button onClick={createProfile}>SEND REQUEST</Button></Row>
                     {profile && <Row>{profile.name}</Row>}
+                    {error && <Row><Col>{error}</Col></Row>}
+
                 </Col>
             </Row>
         </Container>
@@ -55,12 +80,16 @@ export function CreateProfile() {
 export function UpdateProfile() {
 
     const [profile, setProfile] = useState();
+    const [email, setEmail] = useState();
+    const [name, setName] = useState();
+    const [surname, setSurname] = useState();
+    const [error, setError] = useState();
 
     let updateProfile = async () => {
-        let profile;
+        let profile = { email: email, name: name, surname:surname}
         await API.updateProfile(profile)
             .then(data => { profile = data; setProfile(profile)})
-            .catch(error => console.log(error));
+            .catch(error => setError("Error: " + error));
     }
 
     return <>
@@ -68,8 +97,24 @@ export function UpdateProfile() {
             <Row>
                 <Col>
                     <Row><p style={{ textAlign: 'center' }}>PUT /API/profiles/:emailAddress</p></Row>
+                    <Form>
+                            <Form.Group className="mb-3" controlId="email">
+                                <Form.Label>Email</Form.Label>
+                                <Form.Control type="email" placeholder="Enter email" onChange={e => setEmail(e.target.value)} />
+                            </Form.Group>
+                            <Form.Group className="mb-3" controlId="name">
+                                <Form.Label>Name</Form.Label>
+                                <Form.Control type="name" placeholder="Enter name" onChange={e => setName(e.target.value)} />
+                            </Form.Group>
+                            <Form.Group className="mb-3" controlId="surname">
+                                <Form.Label>Surname</Form.Label>
+                                <Form.Control type="surname" placeholder="Enter surname" onChange={e => setSurname(e.target.value)} />
+                            </Form.Group>
+                        </Form>
                     <Row><Button onClick={updateProfile}>SEND REQUEST</Button></Row>
                     {profile && <Row>{profile.name}</Row>}
+                    {error && <Row><Col>{error}</Col></Row>}
+
                 </Col>
             </Row>
         </Container>
