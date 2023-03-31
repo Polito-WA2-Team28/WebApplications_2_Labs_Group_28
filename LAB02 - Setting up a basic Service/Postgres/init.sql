a9bb0609e3c774bb543621c49a51547c79911efd
Edoardo Colella s257113 <s257113@studenti.polito.it>
2023-03-31 17:01:30 +0200

    fix: rename of fields and foreign key on owner_id

--- LAB02 - Setting up a basic Service/Postgres/init.sql
+++ LAB02 - Setting up a basic Service/Postgres/init.sql
@@ -1,21 +1,24 @@
-CREATE TABLE PRODUCT(
-                        serialNumber uuid PRIMARY KEY,
-                        deviceType VARCHAR(255) NOT NULL,
-                        model VARCHAR(255) NOT NULL,
-                        devicePurchaseDate date NOT NULL,
-                        ownerID uuid NOT NULL,
-                        warrantyDescription VARCHAR(255) NOT NULL,
-                        warrantyExpirationDate date NOT NULL,
-                        insurancePurchaseDate date,
-                        insuranceExpirationDate date
-);
-
 CREATE TABLE PROFILE(
-                        profileID uuid PRIMARY KEY,
+                        profile_id uuid PRIMARY KEY,
                         name VARCHAR(255) NOT NULL,
                         surname VARCHAR(255) NOT NULL,
-                        registrationDate date NOT NULL,
-                        birthDate date NOT NULL,
+                        registration_date date NOT NULL,
+                        birth_date date NOT NULL,
                         email VARCHAR(255) NOT NULL,
-                        phoneNumber VARCHAR(255) NOT NULL
+                        phone_number VARCHAR(255) NOT NULL
 );
+
+CREATE TABLE PRODUCT(
+                        serial_number uuid PRIMARY KEY,
+                        device_type VARCHAR(255) NOT NULL,
+                        model VARCHAR(255) NOT NULL,
+                        device_purchase_date date NOT NULL,
+                        owner_id uuid NOT NULL,
+                        warranty_description VARCHAR(255) NOT NULL,
+                        warranty_expirationDate date NOT NULL,
+                        insurance_purchaseDate date,
+                        insurance_expirationDate date,
+                        CONSTRAINT fk_customer
+                        FOREIGN KEY(owner_id) 
+                        REFERENCES PROFILE(profile_id)
+      );
