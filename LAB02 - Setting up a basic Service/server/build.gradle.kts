99c87654e337fadd8e5e433b0eff46a852fc02cc
Manuel Enrique Colotti s297014 <s297014@studenti.polito.it>
2023-04-03 15:49:02 +0200

    feat: added a couple of annotations for http status codes

--- LAB02 - Setting up a basic Service/server/build.gradle.kts
+++ LAB02 - Setting up a basic Service/server/build.gradle.kts
@@ -21,6 +21,7 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
+	implementation("org.hibernate.validator:hibernate-validator")
 	runtimeOnly("org.postgresql:postgresql")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
--- LAB02 - Setting up a basic Service/server/src/main/kotlin/com/lab2/server/controller/ProfileController.kt
+++ LAB02 - Setting up a basic Service/server/src/main/kotlin/com/lab2/server/controller/ProfileController.kt
@@ -3,13 +3,16 @@ package com.lab2.server.controller
 
 import com.lab2.server.dto.ProfileDTO
 import com.lab2.server.service.ProfileServiceImpl
+import jakarta.validation.Valid
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
@@ -17,6 +20,7 @@ class ProfileController @Autowired constructor(val profileService: ProfileServic
 
 
     @GetMapping("/api/profiles/{email}")
+    @ResponseStatus(HttpStatus.OK)
     fun getCustomerById(@PathVariable("email") email:String): ProfileDTO?{
         return profileService.getProfileByEmail(email)
     }
@@ -26,7 +30,7 @@ class ProfileController @Autowired constructor(val profileService: ProfileServic
     //Check if email exists (there should be a repository method that either saves or returns an error)
     //Save
     @PostMapping("/api/profiles")
-    fun addProfile(@RequestBody profile:ProfileDTO){
+    fun addProfile(@RequestBody @Valid profile:ProfileDTO){
 
     }
 
