add0c20d0e2c2b67059a9278ab3a081fe95bf9e9
Davide Arcolini <davidearcolini.99@gmail.com>
2023-05-04 23:21:57 +0200

    feat(ticket): implemented ticket API for manager

--- LAB03 - Ticket management/server/src/main/kotlin/com/lab3/ticketing/controller/TicketManagerController.kt
+++ LAB03 - Ticket management/server/src/main/kotlin/com/lab3/ticketing/controller/TicketManagerController.kt
@@ -1,7 +1,10 @@
 package com.lab3.ticketing.controller
 
 import com.lab3.ticketing.dto.TicketDTO
+import com.lab3.ticketing.exception.Exception
+import com.lab3.ticketing.model.Ticket
 import com.lab3.ticketing.service.TicketServiceImpl
+import com.lab3.ticketing.util.TicketState
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.*
@@ -23,30 +26,83 @@ class TicketManagerController @Autowired constructor(val ticketService: TicketSe
 
     @PatchMapping("/API/managers/{managerId}/tickets/{ticketId}/assign")
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    fun assignTicket(@PathVariable("managerId") managerId:Long,
-                     @PathVariable("ticketId") ticketId:Long){
+    fun assignTicket (
+        @PathVariable("managerId") managerId: Long,
+        @PathVariable("ticketId") ticketId: Long
+    ): TicketDTO? {
+
+        /* retrieve the ticket from the database */
+        val ticket: Ticket = ticketService.getTicketModelById(ticketId)
+            ?: throw Exception.TicketNotFoundException("Ticket not found.")
+
+        /* checking the state of the ticket */
+        if (ticket.state != TicketState.OPEN) {
+            throw Exception.TicketInvalidOperationException("Invalid ticket status for this operation.")
+        }
+
+        /* change the ticket status */
+        return ticketService.changeTicketStatus(ticket, TicketState.IN_PROGRESS)
 
     }
 
     @PatchMapping("/API/managers/{managerId}/tickets/{ticketId}/relieveExpert")
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    fun relieveExpert(@PathVariable("managerId") managerId:Long,
-                      @PathVariable("ticketId") ticketId:Long){
+    fun relieveExpert (
+        @PathVariable("managerId") managerId: Long,
+        @PathVariable("ticketId") ticketId: Long
+    ): TicketDTO? {
 
+        /* retrieve the ticket from the database */
+        val ticket: Ticket = ticketService.getTicketModelById(ticketId)
+            ?: throw Exception.TicketNotFoundException("Ticket not found.")
+
+        /* checking the state of the ticket */
+        if (ticket.state != TicketState.IN_PROGRESS) {
+            throw Exception.TicketInvalidOperationException("Invalid ticket status for this operation.")
+        }
+
+        /* change the ticket status */
+        return ticketService.changeTicketStatus(ticket, TicketState.OPEN)
     }
 
     @PatchMapping("/API/managers/{managerId}/tickets/{ticketId}/close")
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    fun closeTicket(@PathVariable("managerId") managerId:Long,
-                    @PathVariable("ticketId") ticketId:Long){
+    fun closeTicket (
+        @PathVariable("managerId") managerId: Long,
+        @PathVariable("ticketId") ticketId: Long
+    ): TicketDTO? {
 
+        /* retrieve the ticket from the database */
+        val ticket: Ticket = ticketService.getTicketModelById(ticketId)
+            ?: throw Exception.TicketNotFoundException("Ticket not found.")
+
+        /* checking the state of the ticket */
+        if (ticket.state != TicketState.OPEN || ticket.state != TicketState.RESOLVED || ticket.state != TicketState.REOPENED) {
+            throw Exception.TicketInvalidOperationException("Invalid ticket status for this operation.")
+        }
+
+        /* change the ticket status */
+        return ticketService.changeTicketStatus(ticket, TicketState.CLOSED)
     }
 
     @PatchMapping("/API/managers/{managerId}/tickets/{ticketId}/resumeProgress")
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    fun resumeTicketProgress(@PathVariable("managerId") managerId:Long,
-                             @PathVariable("ticketId") ticketId:Long){
+    fun resumeTicketProgress (
+        @PathVariable("managerId") managerId: Long,
+        @PathVariable("ticketId") ticketId: Long
+    ): TicketDTO? {
 
+        /* retrieve the ticket from the database */
+        val ticket: Ticket = ticketService.getTicketModelById(ticketId)
+            ?: throw Exception.TicketNotFoundException("Ticket not found.")
+
+        /* checking the state of the ticket */
+        if (ticket.state != TicketState.REOPENED) {
+            throw Exception.TicketInvalidOperationException("Invalid ticket status for this operation.")
+        }
+
+        /* change the ticket status */
+        return ticketService.changeTicketStatus(ticket, TicketState.IN_PROGRESS)
     }
 
     @DeleteMapping("/API/managers/{managerId}/tickets/{ticketId}/remove")
