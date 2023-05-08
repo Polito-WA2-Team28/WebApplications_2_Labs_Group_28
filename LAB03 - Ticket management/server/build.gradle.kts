563325e6d4555c20934a36a0a06094604c7cf1fb
Edoardo Colella <edoardocolella.it@gmail.com>
2023-05-08 09:44:24 +0200

    update on test

--- LAB03 - Ticket management/server/build.gradle.kts
+++ LAB03 - Ticket management/server/build.gradle.kts
@@ -1,5 +1,7 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
+
+
 plugins {
 	id("org.springframework.boot") version "3.0.5"
 	id("io.spring.dependency-management") version "1.1.0"
@@ -23,12 +25,14 @@ dependencies {
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.hibernate.validator:hibernate-validator")
+	implementation("org.apache.httpcomponents.client5:httpclient5:5.2.1")
+
 	runtimeOnly("org.postgresql:postgresql")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation ("org.testcontainers:junit-jupiter:1.16.3")
 	testImplementation("org.testcontainers:postgresql:1.16.3")
-
 }
+
 
 dependencyManagement {
 	imports {
--- LAB03 - Ticket management/server/src/test/kotlin/com/lab3/server/ServerApplicationTests.kt
+++ LAB03 - Ticket management/server/src/test/kotlin/com/lab3/server/ServerApplicationTests.kt
@@ -1,16 +1,30 @@
 package com.lab3.server
 
+import com.lab3.server.model.Customer
+import com.lab3.server.model.Expert
+import com.lab3.server.model.Product
+import com.lab3.server.repository.CustomerRepository
+import com.lab3.server.repository.ProductRepository
+import com.lab3.ticketing.model.Ticket
 import com.lab3.ticketing.repository.TicketRepository
+import com.lab3.ticketing.util.ExpertiseFieldEnum
+import com.lab3.ticketing.util.TicketState
+import org.json.JSONObject
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.*
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.web.client.TestRestTemplate
 import org.springframework.boot.test.web.server.LocalServerPort
+import org.springframework.http.HttpHeaders
 import org.springframework.http.HttpStatus
+import org.springframework.http.MediaType
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory
 import org.springframework.test.context.*
 import org.testcontainers.containers.PostgreSQLContainer
 import org.testcontainers.junit.jupiter.*
+import java.util.Date
 
 
 @Testcontainers
@@ -34,17 +48,40 @@ class DbT1ApplicationTests{
 	@Autowired
 	lateinit var restTemplate: TestRestTemplate
 
+	@BeforeEach
+	fun setup() {
+		restTemplate.restTemplate.requestFactory = HttpComponentsClientHttpRequestFactory()
+	}
+
 	@Autowired
 	lateinit var ticketRepository: TicketRepository
+	@Autowired
+	lateinit var customerRepository: CustomerRepository
+	@Autowired
+	lateinit var productRepository: ProductRepository
 
 	//	1) /api/customers/:customerId/tickets GET
 
 	@Test
 	fun successAPI1(){
-		val response = restTemplate.getForEntity("/api/customers/1/tickets", String::class.java)
+		ticketRepository.deleteAll()
+		//customerRepository.deleteAll()
+		//productRepository.deleteAll()
+
+		val customer = Customer("Mario", "Rossi", Date(2020,1,1),  Date(1990,1,1),"mario.rossi@mail.com","0123456789")
+		customerRepository.save(customer)
+
+		val expert= Expert("expert01@mail.com", mutableSetOf(ExpertiseFieldEnum.APPLIANCES))
+
+
+		val product = Product("Iphone", "15", 1234, customer)
+		productRepository.save(product)
+
+		val ticket = Ticket(TicketState.OPEN, customer, expert, "Description", product, mutableSetOf(), Date(2020,1,1), Date(2020,1,1))
+		ticketRepository.save(ticket)
+		val response = restTemplate.getForEntity("/API/customers/1/tickets", String::class.java)
 		Assertions.assertNotNull(response)
 		Assertions.assertEquals(HttpStatus.OK, response?.statusCode)
-		Assertions.assertEquals(1,1)
 	}
 
 	//	2) /api/customers/:customerId/ticket POST
@@ -56,54 +93,56 @@ class DbT1ApplicationTests{
 	//	3) /api/experts/:expertId/tickets
 	@Test
 	fun successAPI3(){
-		val response = restTemplate.getForEntity("/api/experts/1/tickets", String::class.java)
+		val response = restTemplate.getForEntity("/API/experts/1/tickets", String::class.java)
 		Assertions.assertNotNull(response)
 		Assertions.assertEquals(HttpStatus.OK, response?.statusCode)
-		Assertions.assertEquals(1,1)
 	}
 
 	//	4) /api/managers/:managerId/tickets
 	@Test
 	fun successAPI4(){
-		val response = restTemplate.getForEntity("/api/managers/1/tickets", String::class.java)
+		val response = restTemplate.getForEntity("/API/managers/1/tickets", String::class.java)
 		Assertions.assertNotNull(response)
 		Assertions.assertEquals(HttpStatus.OK, response?.statusCode)
-		Assertions.assertEquals(1,1)
 	}
 
 	//	5) /api/customers/:customerId/tickets/:ticketId
 	@Test
 	fun successAPI5(){
-		val response = restTemplate.getForEntity("/api/customers/1/tickets/1", String::class.java)
+		val response = restTemplate.getForEntity("/API/customers/1/tickets/1", String::class.java)
 		Assertions.assertNotNull(response)
 		Assertions.assertEquals(HttpStatus.OK, response?.statusCode)
-		Assertions.assertEquals(1,1)
 	}
 
 	//	6) /api/experts/:expertId/tickets/:ticketId
 	@Test
 	fun successAPI6(){
-		val response = restTemplate.getForEntity("/api/experts/1/tickets/1", String::class.java)
+		val response = restTemplate.getForEntity("/API/experts/1/tickets/1", String::class.java)
 		Assertions.assertNotNull(response)
 		Assertions.assertEquals(HttpStatus.OK, response?.statusCode)
-		Assertions.assertEquals(1,1)
 	}
 
 	//	7) /api/managers/:managerId/tickets/:ticketId
 	@Test
 	fun successAPI7(){
-		val response = restTemplate.getForEntity("/api/managers/1/tickets/1", String::class.java)
+		val response = restTemplate.getForEntity("/API/managers/1/tickets/1", String::class.java)
 		Assertions.assertNotNull(response)
 		Assertions.assertEquals(HttpStatus.OK, response?.statusCode)
-		Assertions.assertEquals(1,1)
 	}
 
 	//	8) /api/managers/:managerId/tickets/:ticketId/assign
 	@Test
 	fun successAPI8(){
-		val response = restTemplate.getForEntity("/api/managers/1/tickets/1/assign", String::class.java)
-		Assertions.assertNotNull(response)
-		Assertions.assertEquals(HttpStatus.OK, response?.statusCode)
-		Assertions.assertEquals(1,1)
+
+		val headers = HttpHeaders()
+		headers.contentType = MediaType.APPLICATION_JSON
+		val requestObject= JSONObject()
+		requestObject.put("expertId", 1)
+		println("requestObject $requestObject")
+
+		val response = restTemplate.patchForObject("/API/managers/1/tickets/1/assign", requestObject.toString() ,String::class.java)
+		//Assertions.assertNotNull(response)
+		//Assertions.assertEquals(HttpStatus.OK, response?.statusCode)
+		println("ABECEDARIO $response")
 	}
 }
