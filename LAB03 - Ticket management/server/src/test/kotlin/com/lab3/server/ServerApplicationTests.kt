2bd3917339dbbe2c850803cbc35e1f17bc927d44
Edoardo Colella <edoardocolella.it@gmail.com>
2023-05-08 20:59:49 +0200

    test: all tests on my endpoints work

--- LAB03 - Ticket management/server/src/test/kotlin/com/lab3/server/ServerApplicationTests.kt
+++ LAB03 - Ticket management/server/src/test/kotlin/com/lab3/server/ServerApplicationTests.kt
@@ -47,21 +47,11 @@ class DbT1ApplicationTests {
     @Autowired
     lateinit var restTemplate: TestRestTemplate
 
-    @BeforeClass
-    fun setup() {
-        restTemplate.restTemplate.requestFactory = HttpComponentsClientHttpRequestFactory()
-    }
-
-    @Autowired
-    lateinit var ticketRepository: TicketRepository
-    @Autowired
-    lateinit var customerRepository: CustomerRepository
-    @Autowired
-    lateinit var productRepository: ProductRepository
-    @Autowired
-    lateinit var expertRepository: ExpertRepository
-    @Autowired
-    lateinit var managerRepository: ManagerRepository
+    @Autowired lateinit var ticketRepository: TicketRepository
+    @Autowired lateinit var customerRepository: CustomerRepository
+    @Autowired lateinit var productRepository: ProductRepository
+    @Autowired lateinit var expertRepository: ExpertRepository
+    @Autowired lateinit var managerRepository: ManagerRepository
 
     @BeforeEach
     fun repositoryClean() {
@@ -447,22 +437,49 @@ class DbT1ApplicationTests {
 
     @Ignore
 	@Test /** PATCH /api/managers/:managerId/tickets/:ticketId/assign */
-    fun successapi8() {
+    fun successAssignmentOfATicket() {
+
+        val customer = Customer(
+            "Mario", "Rossi",
+            myDate(2022, 1, 1),
+            myDate(1990, 1, 1),
+            "mario.rossi@mail.com", "0123456789"
+        )
+        customerRepository.save(customer).getId()
+        val expert = Expert("expert01@mail.com", mutableSetOf(ExpertiseFieldEnum.APPLIANCES))
+        val expertId = expertRepository.save(expert).getId()
+        val product = Product("Iphone", "15", 1234, customer)
+        productRepository.save(product).getId()
+
+        val ticket = Ticket(
+            TicketState.OPEN, customer, null, "Description", product, mutableSetOf(),
+            myDate(2020, 1, 1), myDate(2020, 1, 1)
+        )
+        val ticketId = ticketRepository.save(ticket).getId()!!
+
+        val manager = Manager("manager@mail.com")
+        val managerId = managerRepository.save(manager).getId()
 
         val headers = HttpHeaders()
         headers.contentType = MediaType.APPLICATION_JSON
         val requestObject = JSONObject()
-        requestObject.put("expertId", 1)
-        //println("requestObject $requestObject")
+        requestObject.put("expertId", expertId!!.toInt())
 
-        val response = restTemplate.patchForObject(
-            "/api/managers/1/tickets/1/assign",
-            requestObject.toString(),
+        val response = restTemplate.exchange(
+            "/api/managers/${managerId}/tickets/${ticketId}/assign",
+            HttpMethod.PATCH,
+            HttpEntity(requestObject.toString(), headers),
             String::class.java
         )
-        //Assertions.assertNotNull(response)
-        //Assertions.assertEquals(HttpStatus.OK, response?.statusCode)
-        //println("ABECEDARIO $response")
+        Assertions.assertNotNull(response)
+        Assertions.assertEquals(HttpStatus.NO_CONTENT, response.statusCode)
+
+        val actualTicket = ticketRepository.getReferenceById(ticketId)
+        Assertions.assertNotNull(actualTicket.expert)
+        Assertions.assertEquals(actualTicket.expert!!.getId(), expertId )
+        Assertions.assertEquals(actualTicket.state, TicketState.IN_PROGRESS)
+
+
     }
 
 
