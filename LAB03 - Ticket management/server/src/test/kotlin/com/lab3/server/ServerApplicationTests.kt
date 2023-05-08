a265418e8b6db0e90d4824a9d2b950baf4816302
Edoardo Colella <edoardocolella.it@gmail.com>
2023-05-08 08:54:46 +0200

    test class skeleton

--- LAB03 - Ticket management/server/src/test/kotlin/com/lab3/server/ServerApplicationTests.kt
+++ LAB03 - Ticket management/server/src/test/kotlin/com/lab3/server/ServerApplicationTests.kt
@@ -1,22 +1,22 @@
 package com.lab3.server
 
-import org.junit.jupiter.api.Assertions
-import org.junit.jupiter.api.Test
+import com.lab3.ticketing.repository.TicketRepository
+import org.junit.jupiter.api.*
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.web.client.TestRestTemplate
 import org.springframework.boot.test.web.server.LocalServerPort
-import org.springframework.test.context.DynamicPropertyRegistry
-import org.springframework.test.context.DynamicPropertySource
+import org.springframework.http.HttpStatus
+import org.springframework.test.context.*
 import org.testcontainers.containers.PostgreSQLContainer
-import org.testcontainers.junit.jupiter.Container
-import org.testcontainers.junit.jupiter.Testcontainers
+import org.testcontainers.junit.jupiter.*
+
 
 @Testcontainers
 @SpringBootTest(webEnvironment=SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureTestDatabase(replace=AutoConfigureTestDatabase.Replace.NONE)
-class DbT1ApplicationTests {
+class DbT1ApplicationTests{
 	companion object {
 		@Container
 		val postgres = PostgreSQLContainer("postgres:latest")
@@ -34,9 +34,76 @@ class DbT1ApplicationTests {
 	@Autowired
 	lateinit var restTemplate: TestRestTemplate
 
-	@Test
-	fun someTest() {
+	@Autowired
+	lateinit var ticketRepository: TicketRepository
 
+	//	1) /api/customers/:customerId/tickets GET
+
+	@Test
+	fun successAPI1(){
+		val response = restTemplate.getForEntity("/api/customers/1/tickets", String::class.java)
+		Assertions.assertNotNull(response)
+		Assertions.assertEquals(HttpStatus.OK, response?.statusCode)
+		Assertions.assertEquals(1,1)
+	}
+
+	//	2) /api/customers/:customerId/ticket POST
+	@Test
+	fun successAPI2(){
+
+	}
+
+	//	3) /api/experts/:expertId/tickets
+	@Test
+	fun successAPI3(){
+		val response = restTemplate.getForEntity("/api/experts/1/tickets", String::class.java)
+		Assertions.assertNotNull(response)
+		Assertions.assertEquals(HttpStatus.OK, response?.statusCode)
+		Assertions.assertEquals(1,1)
+	}
+
+	//	4) /api/managers/:managerId/tickets
+	@Test
+	fun successAPI4(){
+		val response = restTemplate.getForEntity("/api/managers/1/tickets", String::class.java)
+		Assertions.assertNotNull(response)
+		Assertions.assertEquals(HttpStatus.OK, response?.statusCode)
+		Assertions.assertEquals(1,1)
+	}
+
+	//	5) /api/customers/:customerId/tickets/:ticketId
+	@Test
+	fun successAPI5(){
+		val response = restTemplate.getForEntity("/api/customers/1/tickets/1", String::class.java)
+		Assertions.assertNotNull(response)
+		Assertions.assertEquals(HttpStatus.OK, response?.statusCode)
+		Assertions.assertEquals(1,1)
+	}
+
+	//	6) /api/experts/:expertId/tickets/:ticketId
+	@Test
+	fun successAPI6(){
+		val response = restTemplate.getForEntity("/api/experts/1/tickets/1", String::class.java)
+		Assertions.assertNotNull(response)
+		Assertions.assertEquals(HttpStatus.OK, response?.statusCode)
+		Assertions.assertEquals(1,1)
+	}
+
+	//	7) /api/managers/:managerId/tickets/:ticketId
+	@Test
+	fun successAPI7(){
+		val response = restTemplate.getForEntity("/api/managers/1/tickets/1", String::class.java)
+		Assertions.assertNotNull(response)
+		Assertions.assertEquals(HttpStatus.OK, response?.statusCode)
+		Assertions.assertEquals(1,1)
+	}
+
+	//	8) /api/managers/:managerId/tickets/:ticketId/assign
+	@Test
+	fun successAPI8(){
+		val response = restTemplate.getForEntity("/api/managers/1/tickets/1/assign", String::class.java)
+		Assertions.assertNotNull(response)
+		Assertions.assertEquals(HttpStatus.OK, response?.statusCode)
 		Assertions.assertEquals(1,1)
 	}
 }
